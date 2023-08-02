710eceff92a9b34dee816bca5b58a41be1afb923
zbx1425 <zbx1425@outlook.com>
2023-08-02 11:21:28 +0800

    Fix server and dimension switching

--- common/src/main/java/cn/zbx1425/worldcomment/MainClient.java
+++ common/src/main/java/cn/zbx1425/worldcomment/MainClient.java
@@ -1,5 +1,6 @@
 package cn.zbx1425.worldcomment;
 
+import cn.zbx1425.worldcomment.data.client.ClientDatabase;
 import cn.zbx1425.worldcomment.network.PacketCollectionDataS2C;
 import cn.zbx1425.worldcomment.network.PacketRegionDataS2C;
 import cn.zbx1425.worldcomment.network.PacketEntryUpdateS2C;
@@ -13,6 +14,10 @@ public class MainClient {
 				PacketCollectionDataS2C.IDENTIFIER, PacketCollectionDataS2C.ClientLogics::handle);
 		ClientPlatform.registerNetworkReceiver(
 				PacketEntryUpdateS2C.IDENTIFIER, PacketEntryUpdateS2C.ClientLogics::handle);
+
+		ClientPlatform.registerPlayerJoinEvent(ignored -> {
+			ClientDatabase.INSTANCE.clear();
+		});
 	}
 
 }
